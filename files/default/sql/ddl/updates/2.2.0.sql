26431b6d4cca081a571a4334976cb307e903473b
Fabio Buso <buso.fabio@gmail.com>
2021-02-08 00:09:07 +0100

    move migrations to 2.2.0

--- files/default/sql/ddl/updates/2.2.0.sql
+++ files/default/sql/ddl/updates/2.2.0.sql
@@ -9,3 +9,21 @@ ALTER TABLE `users` DROP COLUMN `security_question`, DROP COLUMN `security_answe
 
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `type`;
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `tag_schema` VARCHAR(13000) NOT NULL DEFAULT '{"type":"string"}';
+
+ALTER TABLE `python_dep` ADD COLUMN `repo_url` VARCHAR(255);
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE `python_dep` `p` SET `repo_url`=(SELECT `url` FROM `anaconda_repo` WHERE `id` = `p`.`repo_id`);
+SET SQL_SAFE_UPDATES = 1;
+
+SET @fk_name = (SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_SCHEMA = "hopsworks" AND TABLE_NAME = "python_dep" AND REFERENCED_TABLE_NAME="anaconda_repo");
+SET @s := concat('ALTER TABLE hopsworks.python_dep DROP FOREIGN KEY `', @fk_name, '`');
+PREPARE stmt1 FROM @s;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+DROP TABLE `anaconda_repo`;
+
+ALTER TABLE `python_dep` DROP INDEX `dependency`;
+ALTER TABLE `python_dep` DROP COLUMN `repo_id`;
+ALTER TABLE `python_dep` ADD CONSTRAINT `dependency` UNIQUE (`dependency`, `version`, `install_type`, `repo_url`);
--- files/default/sql/ddl/updates/undo/2.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.2.0__undo.sql
@@ -52,3 +52,24 @@ ALTER TABLE `users`
 
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `type` varchar(45) NOT NULL DEFAULT 'STRING';
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `tag_schema`;
+
+CREATE TABLE `anaconda_repo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`)
+) ENGINE=ndbcluster AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+INSERT INTO `anaconda_repo`(`url`) SELECT DISTINCT `repo_url` FROM `python_dep`;
+
+ALTER TABLE `python_dep` ADD COLUMN `repo_id` INT(11);
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE `python_dep` `p` SET `repo_id`=(SELECT `id` FROM `anaconda_repo` WHERE `url` = `p`.`repo_url`);
+SET SQL_SAFE_UPDATES = 1;
+
+ALTER TABLE `python_dep` ADD CONSTRAINT `FK_501_510` FOREIGN KEY (`repo_id`) REFERENCES `anaconda_repo` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `python_dep` DROP INDEX `dependency`;
+ALTER TABLE `python_dep` DROP COLUMN `repo_url`;
+ALTER TABLE `python_dep` ADD CONSTRAINT `dependency` UNIQUE (`dependency`, `version`, `install_type`, `repo_id`);
